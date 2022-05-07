7c58efb7c422577ebdd85feaa9f3aabb07a2e2be
AdrianoSantana <ans3@cin.ufpe.br>
2022-05-07 09:22:44 -0300

    feat: add jogadoresController e criarJogadorDTO

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDTO } from './dtos/criar-jogador.dto';
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+  @Post()
+  async CriarAtualizadorJogador(
+    @Body() criarJogadorDTO: CriarJogadorDTO
+  ) {
+    const { email } = criarJogadorDTO
+    return JSON.stringify(`{
+      "email": ${email}
+    }`)
+  }
+}
